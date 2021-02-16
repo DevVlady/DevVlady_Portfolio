d94a5bc86399c4aadf404be006d8eaba0b862ac8
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-02-15 20:19:33 -0600

    Added the code to show that the server is up and running on port.

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const PORT = process.env.PORT || 3000;
+
 const publicPath = path.join(__dirname, './public/index.html');
 
 app.use(express.static(publicPath));
+
+
+
+//Server active on PORT
+app.listen(PORT, () => {
+    console.log(`Server 🌏 is up on PORT ${PORT}!`);
+ });
