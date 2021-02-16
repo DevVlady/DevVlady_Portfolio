a9871fe2a3e7a91b38e378bfb8d1e5e0e79868ab
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-02-15 19:18:50 -0600

    Created the serve.js file which will be used for deploying the site to heroku.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
+// import{ init } from 'emailjs-com';
 import { ContactContainer, FormWrap, FormContent, Form, FormH1, FormLabel, FormInput, FormInputMsg, FormButton, SendIcon } from './ContactElements';
 
 const Contact = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('contact_service', 'contact_form', e.target, 'user_0b8w00fd3suFAUWpKXPNW')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
     return (
         <>
             <ContactContainer id='contact'>
                 <FormWrap>
                     <FormContent>
-                        <Form action="">
+                        <Form className="contact-form" onSubmit={sendEmail}>
                             <FormH1>Contact Me</FormH1>
                             <FormLabel htmlFor='for'>First & Last Name</FormLabel>
-                            <FormInput type='name' required />
+                            <FormInput type="text" name="user_name" required />
                             <FormLabel htmlFor='for'>Email</FormLabel>
-                            <FormInput type='email' required />
+                            <FormInput type="email" name="user_email" required />
                             <FormLabel htmlFor='for'>Message</FormLabel>
-                            <FormInputMsg type='text' required />
-                            <FormButton type='submit'>Send Message <SendIcon/>
+                            <FormInputMsg type='message' name="message" required />
+                            <FormButton type='submit' value="Send">Send Message <SendIcon />
                             </FormButton>
                         </Form>
                     </FormContent>
