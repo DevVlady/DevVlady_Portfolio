ef7bb436bcb4405ab877b6b8c030f4a098a9b96c
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-02-15 20:45:51 -0600

    Changed some of the code to get the deply to heroku to work.

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 // const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 //Root route
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 
 //Server active on PORT
 app.listen(PORT, () => {
-    console.log(`Server 🌏 is up on PORT ${PORT}!`);
+    console.log(`🌏 ==> API server now on PORT ${PORT}!`);
  });
